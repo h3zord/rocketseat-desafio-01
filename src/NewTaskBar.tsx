3e0c573da280dccc413ec86a1fd78bff95de6106
h3zord <lucaschmain@gmail.com>
2023-08-20 15:30:52 -0300

    style: styling input and button

--- src/NewTaskBar.tsx
+++ src/NewTaskBar.tsx
@@ -1,10 +1,15 @@
 import styles from './NewTaskBar.module.css';
+import { PlusCircle } from "@phosphor-icons/react"
 
 export function NewTaskBar() {
   return (
-    <section >
-      <input className={ styles.taskInput } />
-      <button>Criar</button>
+    <section className={ styles.addNewTask }>
+      <input
+        placeholder='Adicione uma nova tarefa'
+      />
+      <button>
+        Criar <PlusCircle size={22}/>
+      </button>
     </section>
   )
 }
---
+++ src/TasksCount.tsx
@@ -0,0 +1,8 @@
+import styles from './TasksCount.module.css';
+
+export function TasksCount() {
+  return (
+    <section className={ styles.tasksCount }>
+    </section>
+  )
+}
